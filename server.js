9994045aa8138d875b321076ac2474e8eee81952
Eric Tovar <tovareric@gmail.com>
2019-05-28 11:17:19 -0400

    updateing and adding user files and views

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+// connect to MongoDB
+require('./config/database');
+
+// load the env var
+require('dotenv').config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
