3545e1b57abf11baafd9a93052df91519e41b5c2
Eric Tovar <tovareric@gmail.com>
2019-05-28 19:30:22 -0400

    addding comments

--- models/user.js
+++ models/user.js
@@ -1,12 +1,18 @@
 var mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
+var commentSchema = new mongoose.Schema({
+    text: String
+}, {
+    timestamps: true
+});
 var userSchema = new mongoose.Schema({
     name: String,
     email: String,
-    cohort: String,
     avatar: String,
-    googleId: String
+    googleId: String,
+    comments: [commentSchema]
+
 
 }, {
     timestamps: true
