35174015fe11ad701613b5cbf2344839c88a281f
Eric Tovar <tovareric@gmail.com>
2019-05-28 11:14:45 -0400

    add database

---
+++ config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const connectionString = 'mongodb://localhost/users';
+
+mongoose.connect(connectionString, {useNewUrlParser: true});
+
+mongoose.connection.on('connected', () => {
+    console.log("connected")
+});
