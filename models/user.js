f4d06eec6835bcaa4a522480bb2b47f782ff25fb
Eric Tovar <tovareric@gmail.com>
2019-05-28 16:20:34 -0400

    adding Oauth and materialize files

---
+++ models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    cohort: String,
+    avatar: String,
+    googleId: String
+
+}, {
+    timestamps: true
+});
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// authentication
+var session = require('express-session');
+var passport = require('passport');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,8 +16,9 @@ var app = express();
 // connect to MongoDB
 require('./config/database');
 
+// connect passport
+require('./config/passport');
 // load the env var
-require('dotenv').config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +29,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// session middleware
+app.use(session({
+  secret: 'secureTheSession',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
