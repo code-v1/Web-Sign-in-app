f04366ef32a7ea5a012a1651169d5ebf1293c9b7
Eric Tovar <tovareric@gmail.com>
2019-05-29 20:16:26 -0500

    connecting the coments

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var commentsRouter = require('./routes/comments');
 
 var app = express();
 require('dotenv').config();
@@ -42,6 +43,7 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
